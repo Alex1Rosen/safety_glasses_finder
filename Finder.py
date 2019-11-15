4b57ccd0e2bda149c9e6e9ec50c5a630ae9f4ed3
Alex1Rosen <50190699+Alex1Rosen@users.noreply.github.com>
2019-11-15 10:21:15 -0800

    V2

--- Finder.py
+++ Finder.py
@@ -12,16 +12,18 @@ cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    glasses = glasses_cascade.detectMultiSCALE(gray, 1.3, 5)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    # glasses = glasses_cascade.detectMultiSCALE(img, 1.3, 5)
    
-    for (ex,ey,ew,eh) in glasses:
-        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        
+        face_cut = img[y:y+h, x:x+w]
+        glasses_in_cut = glasses_cascade.detectMultiSCALE(face_cut, 1.3, 5)
+       for (ex,ey,ew,eh) in glasses_in_cut: 
+            cx=x+ew/2
+            cy=y+eh/2
+            if cx not in range(x,x+w) || cy not in range(y,y+h):
+                cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
         
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
